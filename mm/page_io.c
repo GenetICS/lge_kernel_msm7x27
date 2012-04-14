dd9a8ee15b502d366b4999b3f5cf02e0cd9fae45
Pasquale Lodise <pakilodi@gmail.com>
2012-04-14 13:45:42 +0200

    Add frontswap and enable support

--- mm/page_io.c
+++ mm/page_io.c
@@ -19,6 +19,8 @@
 #include <linux/swapops.h>
 #include <linux/writeback.h>
 #include <asm/pgtable.h>
+#include <linux/frontswap.h>
+#include <linux/swapfile.h>
 
 static struct bio *get_swap_bio(gfp_t gfp_flags,
 				struct page *page, bio_end_io_t end_io)
@@ -98,6 +100,12 @@ int swap_writepage(struct page *page, struct writeback_control *wbc)
 		unlock_page(page);
 		goto out;
 	}
+        if (frontswap_put_page(page) == 0) {
+                set_page_writeback(page);
+                unlock_page(page);
+                end_page_writeback(page);
+                goto out;
+        }
 	bio = get_swap_bio(GFP_NOIO, page, end_swap_bio_write);
 	if (bio == NULL) {
 		set_page_dirty(page);
@@ -122,6 +130,11 @@ int swap_readpage(struct page *page)
 
 	VM_BUG_ON(!PageLocked(page));
 	VM_BUG_ON(PageUptodate(page));
+        if (frontswap_get_page(page) == 0) {
+                SetPageUptodate(page);
+                unlock_page(page);
+                goto out;
+        }
 	bio = get_swap_bio(GFP_KERNEL, page, end_swap_bio_read);
 	if (bio == NULL) {
 		unlock_page(page);
